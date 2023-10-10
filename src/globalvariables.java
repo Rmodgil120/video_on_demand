87fddb6eb33faeb64cce00775db3bde6320aa60a
Rishi Modgil <94107297+Rmodgil120@users.noreply.github.com>
2023-10-10 20:06:58 +0530

    first commit

---
+++ src/globalvariables.java
@@ -0,0 +1,16 @@
+public class globalvariables 
+{
+    
+private int myotp;
+
+    public int getMyotp() {
+        return myotp;
+    }
+
+    public void setMyotp(int myotp) {
+        this.myotp = myotp;
+    }
+
+
+
+}
